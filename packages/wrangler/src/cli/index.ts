51348bd04e14371c895ae14075f7ea723735ec2c
Cass Fridkin <cass@cloudflare.com>
2022-06-15 10:10:08 -0500

    cli.ts -> cli/index.ts

--- packages/wrangler/src/cli/index.ts
+++ packages/wrangler/src/cli/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config"; // Grab locally specified env params from a `.env` file.
 import process from "process";
 import { hideBin } from "yargs/helpers";
-import { FatalError } from "./errors";
-import { main } from ".";
+import { FatalError } from "../errors";
+import { main } from "..";
 
 main(hideBin(process.argv)).catch((e) => {
   // The logging of any error that was thrown from `main()` is handled in the `yargs.fail()` handler.
